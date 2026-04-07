02bfbf6de483819c0ce223d2d627c04becefed21
Tino Hager <tino.hager@nager.at>
2026-04-07 13:56:08 +0200

    Small fixes

--- src/Nager.Moco/Models/InvoicePayment.cs
+++ src/Nager.Moco/Models/InvoicePayment.cs
@@ -3,10 +3,15 @@
     public class InvoicePayment
     {
         public int Id { get; set; }
-        public string Date { get; set; }
-        public decimal PaidTotal { get; set; }
+        public DateOnly Date { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public InvoiceShortInfo Invoice { get; set; }
+        public float PaidTotal { get; set; }
+        public float PaidTotalInAccountCurrency { get; set; }
         public string Currency { get; set; }
-        public string CreatedOn { get; set; }
-        public string UpdatedOn { get; set; }
+        public string Description { get; set; }
+
     }
 }
---
+++ src/Nager.Moco/Models/InvoiceShortInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Nager.Moco.Models
+{
+    public class InvoiceShortInfo
+    {
+        public int Id { get; set; }
+        public string Identifier { get; set; }
+        public string Title { get; set; }
+    }
+}
